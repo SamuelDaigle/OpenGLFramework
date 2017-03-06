a05c898507bb0e9447f959eb2bd0b5b099071a01
Samuel Daigle <Samuel Daigle>
2017-03-06 13:15:18 -0500

    Added new setup files and updated readme.

---
+++ OpenGLFramework/Setup/include/bullet/LinearMath/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/samue/Downloads/bullet3-2.86.1/bullet3-2.86.1/src/LinearMath
+# Build directory: C:/Users/samue/Downloads/bullet3-2.86.1/bullet3-2.86.1/src/LinearMath
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
