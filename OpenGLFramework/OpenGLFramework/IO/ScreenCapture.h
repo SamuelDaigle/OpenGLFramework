90d5a259c67ab5543018d6838e2294bb25611cee
William Valin <frgewiphhe@hotmail.com>
2017-03-09 22:21:05 -0500

    l'export et la projection orthogonal qui fonctionnent #feelsgoodman

--- OpenGLFramework/OpenGLFramework/IO/ScreenCapture.h
+++ OpenGLFramework/OpenGLFramework/IO/ScreenCapture.h
@@ -1,7 +1,6 @@
+#pragma once 
 #include "..\Framework\OpenGL.h"
 #include "..\IO\TextureLoader.h"
-#include "..\Rendering\SkyboxShader.h"
-#include "..\Interface\ICamera.h"
 
 namespace IO {
 	class ScreenCapture
